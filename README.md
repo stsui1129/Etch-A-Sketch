37aeaa57e7e880e2d1c87426c842f5ba0f9bdd08
Sheng <s.tsui@outlook.com>
2020-09-05 20:08:40 +1200

    added random colour generation + additional comments

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ Issues encountered:
 
 Grid didn't generate enough cells to fill the container whenever I changed the gridsize unless the initial gridsize was set to 64(maximum). It took me a while to figure out the loop was not inside the createGrid function, so it was just running on DOM load due to a misplacement of a curly bracket.
 
-Having a border on the cells created a problem where, whenever the grid is reset, extra cells were generated at the bottom of the container altering the size of cells. The problem was fixed by adding: container.removeChild(cell); which removed previously created cells. I'm still not sure why this problem doesn't occur if cells had no border.
+Having a border on the cells created a problem where, whenever the grid was reset, extra cells were generated at the bottom of the container altering the size of cells. The problem was fixed by adding: container.removeChild(cell); which removed previously created cells. I'm still not sure why this problem doesn't occur if cells had no border.
 
 https://stsui1129.github.io/Etch-A-Sketch/
-
-Next update: Implement random colour functioning + improve CSS
--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let gridSize = 16;
 const container = document.querySelector('#container'); //gets reference to container div in our HTML
 const btn = document.querySelector('#clearbtn'); //gets reference to clear grid button and store it in variable btn
 btn.addEventListener('click', clearGrid); //adds a click event to btn which triggers the clearGrid function
+const color = document.querySelector('#color');
+color.addEventListener('click', randomColor);
+const black = document.querySelector('#black');
+black.addEventListener('click', blackColor);
 
 function createGrid(gridSize) {
     //CSS grid repeat function using template literal enclosed by backticks, 1fr = 100% of available space
@@ -14,19 +18,44 @@ function createGrid(gridSize) {
     for (let i = 0; i < (gridSize * gridSize); i++) {
         let cell = document.createElement('div');
         cell.classList.add('cell');
-        cell.addEventListener('mouseover', () => {
-            cell.style.backgroundColor = "black";
-    })
+        cell.addEventListener('mouseover', () => 
+            cell.style.backgroundColor = "black");
+    
         container.appendChild(cell);   
     }
 }
 
+function randomColorGenerator() {
+    let random = `rgb(${Math.floor(Math.random()*255)}, 
+                      ${Math.floor(Math.random()*255)},
+                      ${Math.floor(Math.random()*255)})`;
+    //generates random integers between 0 and 255 for (r, g, b) hence colour
+    return random;
+}
+
+function randomColor() {
+    const cells = document.querySelectorAll('.cell'); //gets reference to node list of class cell
+    //iterates through each cell and adds a mouseover listener which changes colour through randomColorGenerator
+    cells.forEach(cell => { 
+        cell.addEventListener('mouseover', () =>
+            cell.style.backgroundColor = randomColorGenerator())
+    })
+}
+
+function blackColor() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () =>
+            cell.style.backgroundColor = "black")
+    })
+}
+
 function clearGrid() {
     const cells = document.querySelectorAll('.cell');
-        cells.forEach(cell => {
-            cell.style.backgroundColor = ''; //removes background colour of cells
-            container.removeChild(cell); //removes previously created cells
-        })
+    cells.forEach(cell => {
+        cell.style.backgroundColor = ''; //removes background colour of cells
+        container.removeChild(cell); //removes previously created cells
+    })
         
     //container.textContent = ''; also works here
     //Setting this property on a node removes all of its children
@@ -41,11 +70,3 @@ function clearGrid() {
 }
 
 createGrid(gridSize);
-
-
-
-
-
-// Will add random colouring next time
-// `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
-
